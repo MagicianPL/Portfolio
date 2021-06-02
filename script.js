c1825e55bd8f4dcd6ec2428ef2ccddb2e3c0684d
MagicianPL <83660460+MagicianPL@users.noreply.github.com>
2021-06-02 22:48:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const hamburgerIcon = document.getElementById("icon-hamburger");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const x = document.getElementById("closing");
+const oMnie = document.getElementById("about-me");
 const mojeStrony = document.getElementById("moje-strony");
 const vanillaJs = document.getElementById("vanilla-js");
 const allA = document.getElementsByTagName("a");
+
 hamburgerIcon.addEventListener("click", showMenu);
 
 function showMenu() {
-	hamburgerMenu.style.display = "flex";
-	
-	mojeStrony.addEventListener("click", function(){
-		allA.item(0).style.display="flex"
-		allA.item(1).style.display="flex"
-		allA.item(2).style.display="flex"
-		})
-		
-	vanillaJs.addEventListener("click", function(){
-		allA.item(3).style.display="flex"
-		allA.item(4).style.display="flex"
-		allA.item(5).style.display="flex"
-	})
-	
-	x.addEventListener("click", function(){
-		hamburgerMenu.style.display = "none";
-	})
+	hamburgerMenu.style.display = "flex";	
 }
+
+oMnie.addEventListener("click", function(){
+		hamburgerMenu.style.display = "none";
+	});
+	
+mojeStrony.addEventListener("click", function(){
+		if (allA.item(1).style.display == "none") {
+		allA.item(1).style.display="flex";
+		allA.item(2).style.display="flex";
+		allA.item(3).style.display="flex";
+		}
+		
+		else {
+		allA.item(1).style.display="none";
+		allA.item(2).style.display="none";
+		allA.item(3).style.display="none";
+		}
+		});
+		
+vanillaJs.addEventListener("click", function(){
+		if (allA.item(4).style.display =="none") {
+		allA.item(4).style.display="flex";
+		allA.item(5).style.display="flex";
+		}
+		
+		else {
+		allA.item(4).style.display="none";
+		allA.item(5).style.display="none";
+		}
+	});
+	
+x.addEventListener("click", function(){
+		hamburgerMenu.style.display = "none";
+	});
