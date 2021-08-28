caa595896741810316abaa31514c84fd98cb9195
Michał <83660460+MagicianPL@users.noreply.github.com>
2021-08-28 13:57:03 +0200

    Add files via upload
    Better code in JS file + change on css file

--- script.js
+++ script.js
@@ -1,107 +1,59 @@
+const nav = document.querySelector("nav");
+const description = document.querySelector("#description");
+const allPInDesc = document.querySelectorAll("#description p");
+const allDivs = document.querySelectorAll("div");
+const descriptionTwo = allDivs[12];
+const body = document.querySelector("body");
+
+//three variables - three tiles in main site tiles div
+const divLeft = document.querySelector("#Space-Coff");
+const divRight = document.querySelector("#Inwestor");
+const divCenter = document.querySelector("#Wyczesani");
+
+//variables - all tiles in main js tiles div
+const toDo = document.querySelector("#ToDo");
+const colorFlipper = document.querySelector("#Color-Flipper");
+const dice = document.querySelector("#Kostka");
+const magicBall = document.querySelector("#Magic-Ball");
+const stopwatch = document.querySelector("#stopwatch");
+const stickyNotes = document.querySelector("#sticky-notes");
+
+const tiles = document.querySelector("#tiles");
+const tilesJs = document.querySelector("#tiles-js");
+const h2 = document.querySelector("h2"); //this is h2 in tiles (sites tiles)
+
+const footer = document.querySelector("footer");
+
+
+//and all stuff about hamburger case on mobiles or tablets
 const hamburgerIcon = document.getElementById("icon-hamburger");
 const hamburgerMenu = document.getElementById("hamburger-menu");
-const x = document.getElementById("closing");
+const xClosing = document.getElementById("closing");
 const oMnie = document.getElementById("about-me");
 const mojeStrony = document.getElementById("moje-strony");
 const vanillaJs = document.getElementById("vanilla-js");
 const allA = document.getElementsByTagName("a");
 
-const divLeft = document.querySelector("#Space-Coff"); /*divyAll.item(14);*/
-const divRight = document.querySelector("#Inwestor"); /*divyAll.item(18);*/
-const divCenter = document.querySelector("#Wyczesani"); /*divyAll.item(16);*/
-const toDo = document.querySelector("#ToDo"); /*divyAll.item(21);*/
-const colorFlipper = document.querySelector("#Color-Flipper"); /*divyAll.item(23);*/
-const dice = document.querySelector("#Kostka"); /*divyAll.item(25);*/
-const magicBall = document.querySelector("#Magic-Ball");
-const stopwatch = document.querySelector("#stopwatch");
-const stickyNotes = document.querySelector("#sticky-notes");
 
-function showMenu() {
-	hamburgerMenu.style.display = "flex";	
-};
-
-hamburgerIcon.addEventListener("click", showMenu);
-
-oMnie.addEventListener("click", function(){
-		hamburgerMenu.style.display = "none";
-	});
-	
-mojeStrony.addEventListener("click", function(){
-		if (allA.item(1).style.display == "none") {
-		allA.item(1).style.display="flex";
-		allA.item(2).style.display="flex";
-		allA.item(3).style.display="flex";
-		}
-		
-		else {
-		allA.item(1).style.display="none";
-		allA.item(2).style.display="none";
-		allA.item(3).style.display="none";
-		}
-		});
-		
-vanillaJs.addEventListener("click", function(){
-		if (allA.item(4).style.display =="none") {
-		allA.item(4).style.display="flex";
-		allA.item(5).style.display="flex";
-		allA.item(6).style.display="flex";
-		allA.item(7).style.display="flex";
-		allA.item(8).style.display="flex";
-		allA.item(9).style.display="flex";
-		}
-		
-		else {
-		allA.item(4).style.display="none";
-		allA.item(5).style.display="none";
-		allA.item(6).style.display="none";
-		allA.item(7).style.display="none";
-		allA.item(8).style.display="none";
-		allA.item(9).style.display="none";
-		}
-	});
-	
-x.addEventListener("click", function(){
-		hamburgerMenu.style.display = "none";
-	});
-	
-	
-const divyAll = document.getElementsByTagName("div");
-const footer = document.querySelector("footer");
-const opis = divyAll.item(12);
-const nav = document.querySelector("nav");
-const body = document.querySelector("body");
-const description = document.querySelector("#description");
-const allPInDesc = document.querySelectorAll("#description p");
-
-const tiles = document.querySelector("#tiles");
-const tilesJs = document.querySelector("#tiles-js");
-const h2 = document.querySelector("h2");
-
-const translateTiles = () => {
-	dataTilesJs = tilesJs.getBoundingClientRect();
-	tilesY = dataTilesJs.top + dataTilesJs.height / 2;
-	
-	if (tilesY < window.innerHeight) {
-		tiles.classList.add("translate");
-		h2.classList.add("translateh2");
-	};
-};
-
-window.addEventListener("scroll", function() {
-	/*Dodaję przezroczystość do nav, kiedy strona jest scrollowana o jego wysokość*/
-	navHeight = nav.clientHeight;
-	const yNav = body.getBoundingClientRect().top + navHeight;
+/*This function will add opacity to nav when site is scrolled for height of nav.
+Function will be triggered in main function, where I have every functions thas requires trigger by scrolling*/
+const navOpacity = () => {
+	const navHeightInPx = nav.clientHeight;
+	const yNav = body.getBoundingClientRect().top + navHeightInPx; //so it's height of nav in fact
 	if (yNav < 0) {
 		nav.classList.add("opacity");
-	}
-	else {
+	}else {
 		nav.classList.remove("opacity");
 	};
-	
-	/*Ustawiam scalowanie i translatowanie w górę na description*/
+};
+
+//Will blur first description about me when it should - this will be triggered in mainTrigger function
+const blurDescription = () => {
 	const descRect = description.getBoundingClientRect();
 	const descY = descRect.top + descRect.height/7;
+	
 	description.style.transition = "all 2s";
+	
 	for (i=0; i < allPInDesc.length; i++) {
 			allPInDesc[i].style.transition = "all 2s";
 		};
@@ -117,75 +69,140 @@ window.addEventListener("scroll", function() {
 			allPInDesc[i].classList.remove("go-away");
 		};
 	};
+};
+
+//Second description is appearing from bottom - will be triggered in mainTrigger on scrolling
+const showSecondDescription = () => {
+	const descTwoRect = descriptionTwo.getBoundingClientRect();
+	const y = descTwoRect.top + descTwoRect.height /1.5;
 	
+	if (y < window.innerHeight) {
+		descriptionTwo.classList.add("appearing-from-bottom");
+	};
+};
+
+//Appearing tiles in main site tiles div (three divs) - trigger in mainTrigger
+const showSiteTiles = () => {
+	const dataDivLeft = divLeft.getBoundingClientRect();
+	const yDivLeft = dataDivLeft.top + dataDivLeft.height/2;
+	const dataDivRight = divRight.getBoundingClientRect();
+	const yDivRight = dataDivRight.top + dataDivRight.height/2;
+	const dataDivCenter = divCenter.getBoundingClientRect();
+	const yDivCenter = dataDivCenter.top + dataDivCenter.height/2;
 	
-	
-	
-	const dataOpis = opis.getBoundingClientRect();
-	const Y = dataOpis.top + dataOpis.height/1.5;
-	
-	if (Y < window.innerHeight) {
-		opis.classList.add("appearing-from-bottom");
+	if (yDivLeft < window.innerHeight) {
+		divLeft.classList.add("LeftToRight");
 	};
 	
-	
-	
-	const dataDivLeft = divLeft.getBoundingClientRect();
-	const YDivLeft = dataDivLeft.top + dataDivLeft.height/2;
-	const dataDivRight = divRight.getBoundingClientRect();
-	const YDivRight = dataDivRight.top + dataDivRight.height/2;
-	const dataDivCenter = divCenter.getBoundingClientRect();
-	const YDivCenter = dataDivCenter.top + dataDivCenter.height/2;
-	
-	const dataToDo = toDo.getBoundingClientRect();
-	const YToDo = dataToDo.top + dataToDo.height/2;
-	const dataMagicBall = magicBall.getBoundingClientRect();
-	const YMagicBall = dataMagicBall.top + dataMagicBall.height/2;
-	const dataStopwatch = stopwatch.getBoundingClientRect();
-	const YStopwatch = dataStopwatch.top + dataStopwatch.height/2;
-	const dataDice = dice.getBoundingClientRect();
-	const YDice = dataDice.top + dataDice.height/2;
-	const dataFooter = footer.getBoundingClientRect();
-	const YFooter = dataFooter.top + dataFooter.height/2;
-	
-	if (YDivLeft < window.innerHeight) {
-		divLeft.classList.add("LeftToRight");
-	}
-	
-	if (YDivRight < window.innerHeight) {
+	if (yDivRight < window.innerHeight) {
 		divRight.classList.add("RightToLeft");
-	}
+	};
 	
-	if (YDivCenter < window.innerHeight) {
+	if (yDivCenter < window.innerHeight) {
 		divCenter.classList.add("appearing-from-bottom");
-	}
+	};	
+};
+
+//Appearing js tiles in js main div
+const showJsTiles = () => {
+	const dataToDo = toDo.getBoundingClientRect();
+	const yToDo = dataToDo.top + dataToDo.height/2;
+	const dataMagicBall = magicBall.getBoundingClientRect();
+	const yMagicBall = dataMagicBall.top + dataMagicBall.height/2;
+	const dataStopwatch = stopwatch.getBoundingClientRect();
+	const yStopwatch = dataStopwatch.top + dataStopwatch.height/2;
+	const dataDice = dice.getBoundingClientRect();
+	const yDice = dataDice.top + dataDice.height/2;
 	
-	if (YToDo < window.innerHeight) {
+	if (yToDo < window.innerHeight) {
 		toDo.classList.add("LeftToRight");
 		stickyNotes.classList.add("LeftToRight");
 	}
 	
-	if (YMagicBall < window.innerHeight) {
+	if (yMagicBall < window.innerHeight) {
 		magicBall.classList.add("appearing-from-bottom");
-		dice.classList.add("RightToLeft");
+		dice.classList.add("appearing-from-bottom");
 	}
 	
-	if (YStopwatch < window.innerHeight) {
+	if (yStopwatch < window.innerHeight) {
 		stopwatch.classList.add("RightToLeft");
-		colorFlipper.classList.add("appearing-from-bottom");
-	}
+		colorFlipper.classList.add("RightToLeft");
+}
+};
+
+const translateTilesDiv = () => {
+	const dataTilesJs = tilesJs.getBoundingClientRect();
+	const tilesY = dataTilesJs.top + dataTilesJs.height / 3;
+	
+	if (tilesY < window.innerHeight) {
+		tiles.classList.add("translate");
+		h2.classList.add("translateh2");
+	};
+};
+
+//showing footer from bottom - triggered in mainTrigger
+const showFooter = () => {
+	const dataFooter = footer.getBoundingClientRect();
+	const YFooter = dataFooter.top + dataFooter.height/2;
 	
 	if (YFooter < window.innerHeight) {
 		footer.classList.add("appearing-footer");
-	}
+	};
+};
+
+const mainTriggerScrolling = () => {
+	navOpacity();
+	blurDescription();
+	showSecondDescription();
+	showSiteTiles();
+	showJsTiles();
+	translateTilesDiv();
+	showFooter();
+};
+
+//all about hamburger menu
+function showHambMenu() {
+	hamburgerMenu.style.display = "flex";	
+};
+
+window.addEventListener("scroll", mainTriggerScrolling);
+
+hamburgerIcon.addEventListener("click", showHambMenu);
+
+oMnie.addEventListener("click", function(){
+		hamburgerMenu.style.display = "none";
+	}); //because it's anchor for section about me, so I'm closing menu
+
+mojeStrony.addEventListener("click", function(){
+		if (allA.item(1).style.display == "none") {
+		allA.item(1).style.display="flex";
+		allA.item(2).style.display="flex";
+		allA.item(3).style.display="flex";
+		} else {
+		allA.item(1).style.display="none";
+		allA.item(2).style.display="none";
+		allA.item(3).style.display="none";
+		};
+		});
+		
+vanillaJs.addEventListener("click", function(){
+		if (allA.item(4).style.display =="none") {
+		allA.item(4).style.display="flex";
+		allA.item(5).style.display="flex";
+		allA.item(6).style.display="flex";
+		allA.item(7).style.display="flex";
+		allA.item(8).style.display="flex";
+		allA.item(9).style.display="flex";
+		} else {
+		allA.item(4).style.display="none";
+		allA.item(5).style.display="none";
+		allA.item(6).style.display="none";
+		allA.item(7).style.display="none";
+		allA.item(8).style.display="none";
+		allA.item(9).style.display="none";
+		};
+	});
 	
-	translateTiles();
-});
-
-
-
-
-
-
-
-/*17 15 13*/
+xClosing.addEventListener("click", function(){
+		hamburgerMenu.style.display = "none";
+	});
